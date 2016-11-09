c56626b71346ae5871a61a0d041342f7dce9d2d4
florent <dabinflorent@gmail.com>
2016-11-10 00:45:50 +0100

    json parse

---
+++ src/ItiBundle/Controller/ParseJsonController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ItiBundle\Controller;
+
+use ItiBundle\Entity\Data;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ParseJsonController extends Controller
+{
+  /*
+    public function bddAction()
+    {
+        $string = file_get_contents($this->get('kernel')->getRootDir() . "/../web/rdata.json");
+        $jFile = json_decode($string, true);
+
+        $sites = $jFile["features"];
+        $em = $this->getDoctrine()->getManager();
+        foreach ($sites as $site) {
+            $open= new Data();
+            $open->setNom($site["properties"]["nom"]);
+            $open->setAdresse($site["properties"]["adresse"]);
+            $open->setCodepostal($site["properties"]["codepostal"]);
+            $open->setCommune($site["properties"]["commune"]);
+            $open->setTelephone($site["properties"]["telephone"]);
+            $open->setTarifsmin($site["properties"]["tarifsmin"]);
+            $open->setEmail($site["properties"]["email"]);
+            $open->setSiteweb($site["properties"]["siteweb"]);
+            $open->setCoordinates($site["geometry"]["coordinates"][0]);
+            $open->setCoordinates($site["geometry"]["coordinates"][1]);
+
+
+            $em->persist($open);
+        }
+        $em->flush();
+        return $this->render('ItiBundle:ItiView:bdd.html.twig');
+    }
+*/
+}
