2247af3e9e7505dc641d9a448db7d8fd025db1a5
Nagx <k.javazzo@gmail.com>
2016-11-09 17:56:42 +0100

    install fosuserbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new ItiBundle\ItiBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new ItiBundle\ItiBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- src/ItiBundle/Controller/ItiController.php
+++ src/ItiBundle/Controller/ItiController.php
@@ -2,10 +2,16 @@
 
 namespace ItiBundle\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use FOS\UserBundle\FOSUserBundle;
 
 class ItiController extends Controller
 {
@@ -14,7 +20,7 @@ class ItiController extends Controller
         return $this->render('ItiBundle:ItiView:index.html.twig');
     }
 
-    public function userCreateAction()
+    public function userCreateAction(Request $request)
     {
 
     }
